84f9ee7c7b5eab1c4bdcf9c3e7e353cc6a233d29
Tamrazrk <135056938+Tamrazrk@users.noreply.github.com>
2023-07-26 10:59:53 +0400

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,11 @@
+# Existing tuple with integers
+original_tuple = (1, 2, 3, 4)
+
+# Additional integers to add
+additional_integers = (5, 6, 7)
+
+# Create a new tuple by concatenating the original tuple and additional integers
+new_tuple = original_tuple + additional_integers
+
+print("Original tuple:", original_tuple)
+print("New tuple with additional integers:", new_tuple)
