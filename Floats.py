688d998ec4b0ad84eb067371d262aa55956258ff
Tamrazrk <135056938+Tamrazrk@users.noreply.github.com>
2023-07-26 11:01:27 +0400

    Create Floats.py

---
+++ Floats.py
@@ -0,0 +1,5 @@
+# Using a loop to generate a sequence of floats
+float_sequence = [num / 2 for num in range(3, 11)]  # [1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]
+
+# Print the list containing the sequence of floats
+print(float_sequence)
