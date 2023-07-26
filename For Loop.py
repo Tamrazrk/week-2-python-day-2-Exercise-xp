51c5fc9dd3c7c0fecddbffdb14dbe67a1c577ae9
Tamrazrk <135056938+Tamrazrk@users.noreply.github.com>
2023-07-26 11:02:27 +0400

    Create For Loop.py

---
+++ For Loop.py
@@ -0,0 +1,8 @@
+# Using a for loop to print all numbers from 1 to 20, inclusive
+for num in range(1, 21):
+    print(num)
+
+# Using a for loop to print elements with even indices from 1 to 20, inclusive
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
